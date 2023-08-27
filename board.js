49719a35af85c0c6eddf61fddce1d5a44679fa46
bendyoni <bendyoni@gmail.com>
2023-08-27 03:06:42 +0300

    פיתוח של לוח המשחק

---
+++ board.js
@@ -0,0 +1,36 @@
+class Board {
+    constructor() {
+        this.el = document.createElement('div');
+        this.el.className = 'checkers-board';
+
+        this.initialize();
+    }
+
+    initialize() {
+        for (let row = 0; row < 8; row+= 1) {
+            for (let col = 0; col < 8; col+= 1) {
+                const field = this.createField(row, col);
+                this.appendTo(field);
+            }
+        }
+    }
+
+    createField(row, col) {
+        const el = document.createElement('div')
+        el.className = 'checkers-field';
+
+        const dark = Boolean((row + col) % 2);
+        if (dark) {
+            el.className += ' checkers-fieled-dark';
+        }
+        else {
+            el.className += ' checkers-fieled-light';
+        }
+        return el;
+    }
+
+    appendTo(container) {
+        container.appendChild(this.el);
+    }
+
+}
---
+++ checkers.js
@@ -0,0 +1,16 @@
+import Board from './board.js';
+
+class CheckersGame{
+    constructor(container) {
+        this.container = container;
+
+        this.board = new Board();
+        this.board.appendTo(this.container);
+    }
+
+    clear() {
+        this.container.innerHTML = '';
+    }
+}
+
+export default CheckersGame;
